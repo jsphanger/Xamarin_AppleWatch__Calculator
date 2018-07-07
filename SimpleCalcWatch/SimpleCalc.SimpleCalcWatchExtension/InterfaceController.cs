721de586cf225219538b3c9c384546958fb0f005
Joseph Anger <jsphanger@yahoo.com>
2018-07-06 21:31:10 -0500

    Added program files
    Added the initial program files to the repo.

---
+++ SimpleCalcWatch/SimpleCalc.SimpleCalcWatchExtension/InterfaceController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+using WatchKit;
+using Foundation;
+
+namespace SimpleCalc.SimpleCalcWatchExtension
+{
+    public enum CalculationType
+    {
+        None = -1,
+        Add = 0,
+        Subtract = 1,
+        Multiply = 2,
+        Divide = 3,
+        Caluclate = 4
+    }
+
+    public partial class InterfaceController : WKInterfaceController
+    {
+        public string displayString { get; set; }
+        public double total { get; set; }
+        public CalculationType calculation { get; set; }
+
+        public void Calculate()
+        {
+            double currentVal = Convert.ToDouble(displayString);
+
+            //adjust total
+            switch(calculation)
+            {
+                case CalculationType.Add:
+                    total = total + currentVal;
+                    calculation = CalculationType.None;
+                    ClearDisplay();
+                    displayString = total.ToString();
+                    UpdateDisplay();
+                    break;
+                case CalculationType.Subtract:
+                    total = total - currentVal;
+                    calculation = CalculationType.None;
+					ClearDisplay();
+					displayString = total.ToString();
+					UpdateDisplay();
+                    break;
+                case CalculationType.Multiply:
+                    total = total * currentVal;
+                    calculation = CalculationType.None;
+					ClearDisplay();
+					displayString = total.ToString();
+					UpdateDisplay();
+                    break;
+                case CalculationType.Divide:
+
+                    if (currentVal > 0)
+                    {
+                        total = total / currentVal;
+                        calculation = CalculationType.None;
+						ClearDisplay();
+						displayString = total.ToString();
+						UpdateDisplay();
+                    }
+                    break;
+                default:
+                    total = currentVal;
+                    ClearDisplay();
+                    break;
+            }
+
+        }
+        public void UpdateDisplay()
+        {
+            lblCalcDisplay.SetText(displayString); 
+        }
+
+        public void ClearDisplay(){
+            displayString = "";
+            lblCalcDisplay.SetText("");
+        }
+
+        protected InterfaceController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void Awake(NSObject context)
+        {
+            base.Awake(context);
+
+            // Configure interface objects here.
+            Console.WriteLine("{0} awake with context", this);
+        }
+
+        public override void WillActivate()
+        {
+            // This method is called when the watch view controller is about to be visible to the user.
+            Console.WriteLine("{0} will activate", this);
+
+            // init the inital app values
+            total = 0.0d;
+            calculation = CalculationType.None;
+            displayString = "";
+            UpdateDisplay();
+        }
+
+        public override void DidDeactivate()
+        {
+            // This method is called when the watch view controller is no longer visible to the user.
+            Console.WriteLine("{0} did deactivate", this);
+        }
+
+        partial void BtnOne_Activated()
+        {
+            displayString += "1";
+            UpdateDisplay();
+        }
+
+        partial void BtnTwo_Activated()
+        {
+			displayString += "2";
+            UpdateDisplay();
+        }
+
+        partial void BtnThree_Activated()
+		{
+			displayString += "3";
+            UpdateDisplay();
+        }
+
+        partial void BtnFour_Activated()
+		{
+			displayString += "4";
+			UpdateDisplay();
+        }
+
+        partial void BtnFive_Activated()
+		{
+			displayString += "5";
+			UpdateDisplay();
+        }
+
+		partial void BtnSix_Activated()
+		{
+			displayString += "6";
+			UpdateDisplay();
+		}
+
+        partial void BtnSeven_Activated()
+		{
+			displayString += "7";
+			UpdateDisplay();
+        }
+
+        partial void BtnEight_Activated()
+		{
+			displayString += "8";
+			UpdateDisplay();
+        }
+
+        partial void BtnNine_Activated()
+		{
+			displayString += "9";
+			UpdateDisplay();
+        }
+
+        partial void BtnZero_Activated()
+		{
+			displayString += "0";
+			UpdateDisplay();
+        }
+
+        partial void BtnDecimal_Activated()
+		{
+			displayString += ".";
+			UpdateDisplay();
+        }
+
+        partial void BtnDivide_Activated()
+        {
+            Calculate();
+            calculation = CalculationType.Divide;
+        }
+
+        partial void BtnMultiply_Activated()
+        {
+            Calculate();
+            calculation = CalculationType.Multiply;
+        }
+
+        partial void BtnMinus_Activated()
+        {
+            Calculate();
+            calculation = CalculationType.Subtract;
+        }
+
+        partial void BtnAdd_Activated()
+        {
+            Calculate();
+            calculation = CalculationType.Add;
+        }
+
+        partial void BtnEqual_Activated()
+        {
+            Calculate();
+            calculation = CalculationType.None;
+        }
+
+        partial void BtnClear_Activated()
+        {
+			total = 0.0d;
+			calculation = CalculationType.None;
+			ClearDisplay();
+        }
+    }
+}
---
+++ SimpleCalcWatch/SimpleCalc.SimpleCalcWatchExtension/InterfaceController.designer.cs
@@ -0,0 +1,240 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace SimpleCalc.SimpleCalcWatchExtension
+{
+    [Register ("InterfaceController")]
+    partial class InterfaceController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnAdd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnDecimal { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnDivide { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnEight { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnEqual { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnFive { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnFour { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnMinus { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnMultiply { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnNine { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnOne { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnSeven { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnSix { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnThree { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnTwo { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton btnZero { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceLabel lblCalcDisplay { get; set; }
+
+        [Action ("BtnAdd_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnAdd_Activated ();
+
+        [Action ("BtnClear_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnClear_Activated ();
+
+        [Action ("BtnDecimal_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnDecimal_Activated ();
+
+        [Action ("BtnDivide_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnDivide_Activated ();
+
+        [Action ("BtnEight_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnEight_Activated ();
+
+        [Action ("BtnEqual_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnEqual_Activated ();
+
+        [Action ("BtnFive_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnFive_Activated ();
+
+        [Action ("BtnFour_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnFour_Activated ();
+
+        [Action ("BtnMinus_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnMinus_Activated ();
+
+        [Action ("BtnMultiply_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnMultiply_Activated ();
+
+        [Action ("BtnNine_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnNine_Activated ();
+
+        [Action ("BtnOne_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnOne_Activated ();
+
+        [Action ("BtnSeven_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnSeven_Activated ();
+
+        [Action ("BtnSix_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnSix_Activated ();
+
+        [Action ("BtnThree_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnThree_Activated ();
+
+        [Action ("BtnTwo_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnTwo_Activated ();
+
+        [Action ("BtnZero_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnZero_Activated ();
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnAdd != null) {
+                btnAdd.Dispose ();
+                btnAdd = null;
+            }
+
+            if (btnDecimal != null) {
+                btnDecimal.Dispose ();
+                btnDecimal = null;
+            }
+
+            if (btnDivide != null) {
+                btnDivide.Dispose ();
+                btnDivide = null;
+            }
+
+            if (btnEight != null) {
+                btnEight.Dispose ();
+                btnEight = null;
+            }
+
+            if (btnEqual != null) {
+                btnEqual.Dispose ();
+                btnEqual = null;
+            }
+
+            if (btnFive != null) {
+                btnFive.Dispose ();
+                btnFive = null;
+            }
+
+            if (btnFour != null) {
+                btnFour.Dispose ();
+                btnFour = null;
+            }
+
+            if (btnMinus != null) {
+                btnMinus.Dispose ();
+                btnMinus = null;
+            }
+
+            if (btnMultiply != null) {
+                btnMultiply.Dispose ();
+                btnMultiply = null;
+            }
+
+            if (btnNine != null) {
+                btnNine.Dispose ();
+                btnNine = null;
+            }
+
+            if (btnOne != null) {
+                btnOne.Dispose ();
+                btnOne = null;
+            }
+
+            if (btnSeven != null) {
+                btnSeven.Dispose ();
+                btnSeven = null;
+            }
+
+            if (btnSix != null) {
+                btnSix.Dispose ();
+                btnSix = null;
+            }
+
+            if (btnThree != null) {
+                btnThree.Dispose ();
+                btnThree = null;
+            }
+
+            if (btnTwo != null) {
+                btnTwo.Dispose ();
+                btnTwo = null;
+            }
+
+            if (btnZero != null) {
+                btnZero.Dispose ();
+                btnZero = null;
+            }
+
+            if (lblCalcDisplay != null) {
+                lblCalcDisplay.Dispose ();
+                lblCalcDisplay = null;
+            }
+        }
+    }
+}
